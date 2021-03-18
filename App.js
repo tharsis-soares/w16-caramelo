eb279b34de24171f7624ea422e2564dd1065b59c
tharsis-soares <50808840+tharsis-soares@users.noreply.github.com>
2021-03-18 06:39:17 -0300

    W16-Warker Teste de Aplicativo Google Maps

--- App.js
+++ App.js
@@ -1,21 +1,112 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import MapView from 'react-native-maps';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { BreadProvider, Button } from 'material-bread'
+import { Formik } from 'formik'
+
+function DetailsScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Details!</Text>
+    </View>
+  );
+}
+
+function HomeScreen({ navigation }) {
+  return (
+    <View style={ styles.container }>
+        <Button
+          text="Go to Details"
+          type='contained'
+          color={'#009688'}
+          onPress={() => navigation.navigate('Details')}
+        />
+        <Button
+          text="Go to Details"
+          type='contained'
+          color={'#009688'}
+          onPress={() => navigation.navigate('Details')}
+        />
+        <Button
+          text="Go to Details"
+          type='contained'
+          color={'#009688'}
+          onPress={() => navigation.navigate('Details')}
+        />
+    </View>
+  );
+}
+
+function SettingsScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1 }}>
+      <MapView
+          style={{flex: 1}}
+          provider={MapView.PROVIDER_GOOGLE}
+          region={{
+            latitude: -27.676497294,
+            longitude: -48.48833138,
+            latitudeDelta: 0.0143,
+            longitudeDelta: 0.0134,
+          }}
+          showsUserLocation
+          loadingEnabled
+        >
+        </MapView>
+      <Text>Settings screen</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate('Details')}
+      />
+    
+    </View>
+  );
+}
+
+const HomeStack = createStackNavigator();
+
+function HomeStackScreen() {
+  return (
+    <HomeStack.Navigator>
+      <HomeStack.Screen name="Home" component={HomeScreen} />
+      <HomeStack.Screen name="Details" component={DetailsScreen} />
+    </HomeStack.Navigator>
+  );
+}
+
+const SettingsStack = createStackNavigator();
+
+function SettingsStackScreen() {
+  return (
+    <SettingsStack.Navigator>
+      <SettingsStack.Screen name="Settings" component={SettingsScreen} />
+      <SettingsStack.Screen name="Details" component={DetailsScreen} />
+    </SettingsStack.Navigator>
+  );
+}
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <BreadProvider>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeStackScreen} />
+        <Tab.Screen name="Settings" component={SettingsStackScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+    </BreadProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
-});
+  }
+})
